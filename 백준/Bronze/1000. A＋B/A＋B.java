1ec443590da6223a53af659b40dcb51fc8f61e43
DragonB <59592897+dydqh@users.noreply.github.com>
2022-07-01 14:31:16 +0900

    [Bronze V] Title: A+B, Time: 80 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,11 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-		sc.close();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = br.readLine();
+
+		int num1 = Integer.parseInt(input.split(" ")[0]);
+		int num2 = Integer.parseInt(input.split(" ")[1]);
 
 		int num3 = num1 + num2;
 		System.out.print(num3);

61f46a43cc09e362609418af699a7eb14d2d8297
DragonB <59592897+dydqh@users.noreply.github.com>
2022-07-22 15:11:00 +0900

    [Bronze V] Title: A×B, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = br.readLine();
+
+		int num1 = Integer.parseInt(input.split(" ")[0]);
+		int num2 = Integer.parseInt(input.split(" ")[1]);
+
+		int num3 = num1 * num2;
+		System.out.print(num3);
+	}
+}

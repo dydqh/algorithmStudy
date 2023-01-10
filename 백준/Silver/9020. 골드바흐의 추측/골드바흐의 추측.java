91514051cd6e7ed865bf312e03117171be52ec0a
dydqh <59592897+dydqh@users.noreply.github.com>
2023-01-10 14:34:59 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 700 ms, Memory: 78156 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        StringBuffer sb = new StringBuffer();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        List<Integer> primeList = new ArrayList<>();
+        boolean[] isPrime = new boolean[10001];
+
+        for(int i = 2 ; i < 10001 ; i++){
+            if(isPrime[i]){
+                continue;
+            }
+
+            primeList.add(i);
+            int target = i;
+            int index = 1;
+            while(target * index < 10001){
+                isPrime[target * index] = true;
+                index += 1;
+            }
+        }
+
+        for(int i = 0 ; i < count ; i++){
+            int T = Integer.parseInt(br.readLine());
+            int[] tempList = primeList.stream().filter(x -> (x <= T / 2)).sorted().mapToInt(Integer::intValue).toArray();
+
+            for(int j = tempList.length - 1 ; j >= 0 ; j--){
+                if(primeList.indexOf(T - tempList[j]) != -1){
+                    int min = tempList[j] > T - tempList[j] ? T - tempList[j] : tempList[j];
+                    int max = tempList[j] < T - tempList[j] ? T - tempList[j] : tempList[j];
+                    sb.append(min).append(" ").append(max).append("\n");
+                    break;
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}

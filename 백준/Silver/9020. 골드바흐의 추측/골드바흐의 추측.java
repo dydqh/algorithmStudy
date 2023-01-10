e896acf157c1ca55406f39736c5fa38de491d23f
dydqh <59592897+dydqh@users.noreply.github.com>
2023-01-10 15:19:03 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 116 ms, Memory: 13320 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -3,42 +3,35 @@ import java.util.*;
 
 class Main{
     public static void main(String[] args) throws IOException{
+        int size = 10000;
+
         StringBuffer sb = new StringBuffer();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(br.readLine());
-        List<Integer> primeList = new ArrayList<>();
-        boolean[] isPrime = new boolean[10001];
+        boolean[] isPrime = new boolean[size+ 1];
 
-        for(int i = 2 ; i < 10001 ; i++){
-            if(isPrime[i]){
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for(int i = 2 ; i <= Math.sqrt(size) ; i++){
+            if(!isPrime[i]){
                 continue;
             }
 
-            primeList.add(i);
             int target = i;
-            int index = 1;
-            while(target * index < 10001){
-                isPrime[target * index] = true;
+            int index = 2;
+            while(target * index < (size + 1)){
+                isPrime[target * index] = false;
                 index += 1;
             }
         }
 
         for(int i = 0 ; i < count ; i++){
             int T = Integer.parseInt(br.readLine());
-            // int[] tempList = primeList.stream().filter(x -> (x <= T / 2)).sorted().mapToInt(Integer::intValue).toArray();
-
-            // for(int j = tempList.length - 1 ; j >= 0 ; j--){
-            //     if(primeList.indexOf(T - tempList[j]) != -1){
-            //         int min = tempList[j] > T - tempList[j] ? T - tempList[j] : tempList[j];
-            //         int max = tempList[j] < T - tempList[j] ? T - tempList[j] : tempList[j];
-            //         sb.append(min).append(" ").append(max).append("\n");
-            //         break;
-            //     }
-            // }
             int left = T / 2;
             int right = T / 2;
             while(true){
-                if((primeList.indexOf(left) != -1) && (primeList.indexOf(right) != -1)){
+                if(isPrime[left] && isPrime[right]){
                     sb.append(left).append(" ").append(right).append("\n");
                     break;
                 }

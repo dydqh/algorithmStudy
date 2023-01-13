d14c7e13587977275a544b6d70a04472d9611178
dydqh <59592897+dydqh@users.noreply.github.com>
2023-01-13 14:25:00 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 452 ms, Memory: 55604 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -8,12 +8,12 @@ class Main{
         String[] input = br.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
         int M = Integer.parseInt(input[1]);
-        Map<Integer, String> stringMap = new HashMap<>();
+        String[] stringList = new String[N + 1];
         Map<String, Integer> integerMap = new HashMap<>();
 
         for(int i = 1 ; i <= N ; i++){
             String temp = br.readLine();
-            stringMap.put(i, temp);
+            stringList[i] = temp;
             integerMap.put(temp, i);
         }
 
@@ -22,7 +22,7 @@ class Main{
             boolean isNumber = question.charAt(0) >= 48 && question.charAt(0) <= 57 ? true : false;
 
             if(isNumber){
-                sb.append(stringMap.get(Integer.valueOf(question))).append("\n");
+                sb.append(stringList[Integer.valueOf(question)]).append("\n");
             }
             else{
                 sb.append(integerMap.get(question)).append("\n");

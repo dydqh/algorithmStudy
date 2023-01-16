a237a34526c9a27499b7db128c6192be7627ddf0
dydqh <59592897+dydqh@users.noreply.github.com>
2023-01-16 15:39:11 +0900

    [Silver III] Title: N과 M (2), Time: 88 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+class Main{
+    public static int[] arr;
+    public static StringBuffer sb = new StringBuffer();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputNM = br.readLine().split(" ");
+        int N = Integer.parseInt(inputNM[0]);
+        int M = Integer.parseInt(inputNM[1]);
+        
+        arr = new int[M];
+
+        dfs(N, M, 0, 1);
+    }
+
+    public static void dfs(int N, int M, int depth, int at) {
+        if(M == depth){
+            for(int item : arr){
+                sb.append(item).append(" ");
+            }
+            System.out.println(sb.toString());
+            sb.setLength(0);
+            return;
+        }
+        else{
+            for(int i = at ; i <= N ; i++){
+                arr[depth] = i;
+                dfs(N, M, depth + 1, i + 1);
+            }
+        }
+    }
+}

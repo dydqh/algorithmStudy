5c8ac8e1645f0f2e805516bb52e8ba77f6eafed5
dydqh <59592897+dydqh@users.noreply.github.com>
2023-01-30 13:56:41 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 84 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -4,35 +4,34 @@ import java.util.*;
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> list = new ArrayList<>();
         int N = Integer.parseInt(br.readLine());
         String[] numbers = br.readLine().split(" ");
+        int[] list = new int[1001];
+        list[0] = Integer.parseInt(numbers[0]);
+        int size = 1;
 
         for(int i = 0 ; i < N ; i++){
             int curNumber = Integer.parseInt(numbers[i]);
-            if(i == 0){
-                list.add(curNumber);
-            }
-            else{
-                for(int j = list.size() - 1 ; j >= 0 ; j--){
-                    if(list.get(j) < curNumber){
-                        if(j == list.size() - 1){
-                            list.add(curNumber);
-                            break;
-                        }
-                        else{
-                            list.set(j + 1, curNumber);
-                            break;
-                        }
-                    }
 
-                    if(j == 0){
-                        list.set(0, curNumber);
+            for(int j = size - 1 ; j >= 0 ; j--){
+                if(list[j] < curNumber){
+                    if(j == size - 1){
+                        list[size] = curNumber;
+                        size += 1;
+                        break;
                     }
+                    else{
+                        list[j + 1] = curNumber;
+                        break;
+                    }
+                }
+
+                if(j == 0){
+                    list[0] = curNumber;
                 }
             }
         }
 
-        System.out.println(list.size());
+        System.out.println(size);
     }
 }

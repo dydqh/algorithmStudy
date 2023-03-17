044b7cf598e39fde4bd2317a665629ac613275b4
dydqh <59592897+dydqh@users.noreply.github.com>
2023-03-17 11:15:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        long A = Long.parseLong(input[0]);
+        long B = Long.parseLong(input[1]);
+        long C = Long.parseLong(input[2]);
+        
+        System.out.println(A + B + C);
+    }
+}

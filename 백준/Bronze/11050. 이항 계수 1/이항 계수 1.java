1a6e2871b341af9528c2e750daafb9d510ccd6ec
dydqh <59592897+dydqh@users.noreply.github.com>
2023-03-22 13:18:40 +0900

    [Bronze I] Title: 이항 계수 1, Time: 80 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,20 +1,28 @@
 import java.io.*;
 
 class Main{
+    public static int[][] dp;
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
         int K = Integer.parseInt(input[1]);
 
+        dp = new int[N + 1][K + 1];
+        
         System.out.println(BC(N, K));
     }
 
     public static int BC(int N, int K){
+        if(dp[N][K] > 0){
+            return dp[N][K];
+        }
+        
         if(N == K || K == 0){
-            return 1;
+            return dp[N][K] = 1;
         }
 
-        return BC(N - 1, K - 1) + BC(N - 1, K);
+        return dp[N][K] = (BC(N - 1, K - 1) + BC(N - 1, K));
     }
 }

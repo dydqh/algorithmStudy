520cc7be031d2c048d72bd93595d9429a7ad8e15
dydqh <59592897+dydqh@users.noreply.github.com>
2023-04-16 19:46:55 +0900

    [Silver V] Title: 창문 닫기, Time: 80 ms, Memory: 11476 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.java
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -4,18 +4,6 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(br.readLine());
-        int result = 0;
-        int index = 1;
-
-        while(true){
-            result++;
-            index++;
-            int curIndex = index * index;
-            if(curIndex > number){
-                break;
-            }
-        }
-
-        System.out.println(result);
+        System.out.println((int)Math.sqrt(number));
     }
 }

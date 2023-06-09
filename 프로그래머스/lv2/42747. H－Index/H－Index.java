5494aa05b9b0b57848ef3ef42f64f624efb09e17
dydqh <59592897+dydqh@users.noreply.github.com>
2023-06-09 21:53:55 +0900

    [level 2] Title: H-Index, Time: 0.55 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        int count = citations.length;
+        
+        for(int i = 0 ; i < citations.length ; i++){
+            if(citations[i] >= count){
+                break;
+            }
+            count--;
+        }
+        
+        return count;
+    }
+}

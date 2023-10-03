026c543d1596dd9185e6f97c36ba20f0b4bed7d7
dydqh <59592897+dydqh@users.noreply.github.com>
2023-10-03 16:15:06 +0900

    [unrated] Title: 접미사 배열, Time: 2.25 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        List<String> list = new ArrayList<>();
+        for(int i = 0 ; i < my_string.length() ; i++){
+            list.add(my_string.substring(i, my_string.length()));
+        }
+        return list.stream().sorted().map(o -> o.toString()).toArray(String[]::new);
+    }
+}

929508bc8c07b54c60f628889800eb21c594253e
dydqh <59592897+dydqh@users.noreply.github.com>
2023-10-05 10:06:10 +0900

    [unrated] Title: 카운트 다운, Time: 1.89 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int start, int end_num) {
+        return IntStream.rangeClosed(end_num, start).map(i -> start + end_num - i).toArray();
+    }
+}

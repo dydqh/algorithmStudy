0fe199ea553edc8c6ee65b99e72d16eeefe1948c
dydqh <59592897+dydqh@users.noreply.github.com>
2023-11-03 23:30:39 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 8.50 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        return IntStream.range(0, arr.length).map(i -> k % 2 == 0 ? arr[i] + k : arr[i] * k).toArray();
+    }
+}

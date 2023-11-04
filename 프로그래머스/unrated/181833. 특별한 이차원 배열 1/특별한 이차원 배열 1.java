56a374b0b52ee3ed49bc5ab7c296a6734890ad10
dydqh <59592897+dydqh@users.noreply.github.com>
2023-11-04 20:13:58 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 1.18 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        IntStream.range(0, n).forEach(i -> answer[i][i] = 1);
+        return answer;
+    }
+}

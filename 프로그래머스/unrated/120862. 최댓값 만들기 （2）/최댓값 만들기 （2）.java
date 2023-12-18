5a875742d88fe2878268654518cba4040b96ddb9
dydqh <59592897+dydqh@users.noreply.github.com>
2023-12-18 13:22:04 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 2.90 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -3,8 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
         int[] temp = Arrays.stream(numbers).sorted().toArray();
-        int a = temp[0] * temp[1];
-        int b = temp[temp.length - 1] * temp[temp.length - 2];
-        return a > b ? a : b;
+        return Math.max(temp[0] * temp[1], temp[temp.length - 1] * temp[temp.length - 2]);
     }
 }

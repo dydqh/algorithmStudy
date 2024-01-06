01c50c8186e05186aa797aa98ee0978d269939be
dydqh <59592897+dydqh@users.noreply.github.com>
2024-01-06 23:52:11 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int odd = 0;
+        int even = 0;
+        
+        for(int i = 0 ; i < num_list.length ; i++){
+            if(num_list[i] % 2 == 0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
+        
+        return new int[]{even, odd};
+    }
+}

a0030693c04123697ae6f5ed7b1d4eb7130a05b3
dydqh <59592897+dydqh@users.noreply.github.com>
2024-01-08 18:01:52 +0900

    [level unrated] Title: 한 번만 등장한 문자, Time: 7.61 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -1,10 +1,8 @@
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 class Solution {
     public String solution(String s) {
-        int[] count = new int[123];
-        s.chars().forEach(i -> count[i]++);
-        return IntStream.range(97, 123).filter(i -> count[i] == 1).sorted().mapToObj(i -> (char)i + "").collect(Collectors.joining());
+        return Arrays.stream(s.split("")).collect(Collectors.groupingBy(sp -> sp)).entrySet().stream().filter(entry -> entry.getValue().size() <= 1).map(Map.Entry::getKey).sorted().collect(Collectors.joining());
     }
 }

ce15ff74d53798255e95094f9a5a7386babe3c7d
dydqh <59592897+dydqh@users.noreply.github.com>
2024-01-24 09:26:25 +0900

    [level 0] Title: A로 B 만들기, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,15 +1,9 @@
+import java.util.*;
+
 class Solution {
     public int solution(String before, String after) {
-        int answer = 1;
-        int[] count = new int[26];
-        for(char bC : before.toCharArray()) count[bC - 97]++;
-        for(char aC : after.toCharArray()) count[aC - 97]--;
-        for(int check : count){
-            if(check != 0){
-                answer = 0;
-                break;
-            }
-        }
+        List<Character> list = new ArrayList<>();
+        int answer = 0;
         return answer;
     }
 }

7fb9f1773f14cef16f66af2a784103747e10c950
dydqh <59592897+dydqh@users.noreply.github.com>
2024-02-13 17:54:29 +0900

    [level 0] Title: 암호 해독, Time: 2.49 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,7 @@
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String cipher, int code) {
+        return IntStream.range(0, cipher.length()).filter(i -> (i + 1) % code == 0).mapToObj(i -> String.valueOf(cipher.charAt(i))).collect(Collectors.joining());
+    }
+}

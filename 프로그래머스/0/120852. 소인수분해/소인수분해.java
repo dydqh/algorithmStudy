288af8ecfe4fe894add3f7dd6eff6dcd769d3232
dydqh <59592897+dydqh@users.noreply.github.com>
2024-02-19 17:59:52 +0900

    [level 0] Title: 소인수분해, Time: 2.37 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -2,31 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        Set<Integer> answer = new HashSet<>();
-        List<Integer> prime = new ArrayList<>();
-        boolean[] isPrime = new boolean[n + 1];
-        Arrays.fill(isPrime, true);
-        isPrime[0] = false;
-        isPrime[1] = false;
+        Set<Integer> set = new HashSet<>();
         for(int i = 2 ; i <= n ; i++){
-            if(isPrime[i]){
-                prime.add(i);
-                int index = 2;
-                while(i * index <= n){
-                    isPrime[i * index] = false;
-                    index++;
-                }
+            if(n % i == 0){
+                n /= i;
+                set.add(i);
+                i--;
             }
         }
-        while(n > 1){
-            for(int curPrime : prime){
-                if(n % curPrime == 0){
-                    n /= curPrime;
-                    answer.add(curPrime);
-                    break;
-                }
-            }
-        }
-        return answer.stream().sorted().mapToInt(i -> i).toArray();
+        return set.stream().sorted().mapToInt(i -> i).toArray();
     }
 }

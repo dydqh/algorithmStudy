fc75c5a6e09b3d2548e7c47f4fc6213f088e000b
dydqh <59592897+dydqh@users.noreply.github.com>
2024-03-18 15:45:41 +0900

    [Silver I] Title: 후보 추천하기, Time: 264 ms, Memory: 18692 KB -BaekjoonHub

---
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int count = Integer.parseInt(br.readLine());
+        List<Integer> rank = new ArrayList<>();
+        Map<Integer, Integer> score = new HashMap<>();
+        Map<Integer, Integer> order = new HashMap<>();
+        String[] students = br.readLine().split(" ");
+        for(int i = 0 ; i < students.length ; i++){
+            int student = Integer.parseInt(students[i]);
+
+            if(!rank.contains(student)){
+                if(rank.size() >= n){
+                    score.remove(rank.get(n - 1));
+                    order.remove(rank.get(n - 1));
+                    rank.remove(n - 1);
+                }
+
+                rank.add(student);
+                score.put(student, 1);
+                order.put(student, i);
+            }
+            else{
+                score.put(student, score.get(student) + 1);
+            }
+
+            order(rank, score, order, student);
+        }
+        rank.stream().sorted().forEach(i -> sb.append(i).append(" "));
+        System.out.println(sb.toString());
+    }
+
+    public static void order(List<Integer> rank, Map<Integer, Integer> score, Map<Integer, Integer> order, int student){
+        int index = rank.indexOf(student);
+        while(index > 0){
+            int targetStudent = rank.get(index - 1);
+            if(score.get(student) > score.get(targetStudent)){
+                swap(rank, index, index - 1);
+            }
+            else if(score.get(student) == score.get(targetStudent) && order.get(student) > order.get(targetStudent)){
+                swap(rank, index, index - 1);
+            }
+            else{
+                break;
+            }
+            index = rank.indexOf(student);
+        }
+    }
+
+    public static void swap(List<Integer> rank, int i, int j){
+        int temp = rank.get(i);
+        rank.set(i, rank.get(j));
+        rank.set(j, temp);
+    }
+}

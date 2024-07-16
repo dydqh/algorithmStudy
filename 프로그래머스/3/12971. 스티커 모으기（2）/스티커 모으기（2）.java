298a34edf0c1af36b110217a75d26db3ac8a2ee9
dydqh <59592897+dydqh@users.noreply.github.com>
2024-07-16 10:57:53 +0900

    [level 3] Title: 스티커 모으기(2), Time: 6.71 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -12,19 +12,20 @@ class Solution {
         else{
             int[] dp = new int[len];
             int[] dp2 = new int[len];
+            
             dp[0] = sticker[0];
             dp[1] = Math.max(sticker[0], sticker[1]);
             for (int i = 2; i < len - 1; i++) {
                 dp[i] = Math.max(dp[i - 1] , dp[i - 2] + sticker[i]);
             }
+            
             dp2[0] = 0;
             dp2[1] = sticker[1];
             for (int i = 2; i < len; i++) {
                 dp2[i] = Math.max(dp2[i - 1] , dp2[i - 2] + sticker[i]);
             }
-            int dpMax = Math.max(dp[len - 2], dp[len - 3]);
-            int dp2Max = Math.max(dp2[len - 1], dp2[len - 2]);
-            answer = Math.max(dpMax, dp2Max);    
+            
+            answer = Math.max(dp[len - 2], dp2[len - 1]);
         }
         return answer;
     }

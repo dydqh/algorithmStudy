be26e2159c4109d3ad63bfb7823c658a9b6ba138
dydqh <59592897+dydqh@users.noreply.github.com>
2024-10-08 14:06:29 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 603.52 ms, Memory: 1.07 GB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] points, int[][] routes) {
+        int answer = 0;
+        int[][][] map = new int[20000][101][101];
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < routes.length; i++) {
+            int[] route = routes[i];
+            int[] start = points[route[0] - 1];
+            int[] end = points[route[1] - 1];
+            int endIndex = 1;
+            map[0][start[0]][start[1]]++;
+            queue.add(new int[]{start[0], start[1], end[0], end[1], endIndex, i, 0});
+            if (map[0][start[0]][start[1]] == 2) {
+                answer++;
+            }
+        }
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            cur[6]++;
+
+            if(cur[0] != cur[2]){
+                if(cur[0] > cur[2]){
+                    cur[0]--;
+                }
+                else{
+                    cur[0]++;
+                }
+            }
+            else if(cur[1] != cur[3]){
+                if(cur[1] > cur[3]){
+                    cur[1]--;
+                }
+                else{
+                    cur[1]++;
+                }
+            }
+
+            map[cur[6]][cur[0]][cur[1]]++;
+            if (map[cur[6]][cur[0]][cur[1]] == 2) {
+                answer++;
+            }
+
+            if(cur[0] == cur[2] && cur[1] == cur[3]){
+                if(routes[cur[5]].length - 1 > cur[4]){
+                    cur[0] = points[routes[cur[5]][cur[4]] - 1][0];
+                    cur[1] = points[routes[cur[5]][cur[4]] - 1][1];
+                    cur[4]++;
+                    cur[2] = points[routes[cur[5]][cur[4]] - 1][0];
+                    cur[3] = points[routes[cur[5]][cur[4]] - 1][1];
+                    queue.add(cur);
+                }
+            }
+            else{
+                queue.add(cur);
+            }
+        }
+        return answer;
+    }
+}

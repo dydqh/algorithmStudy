c1e3e9ff3b91c4e4ede7b2fc480d19e154f10412
dydqh <59592897+dydqh@users.noreply.github.com>
2025-02-21 11:26:29 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.16 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -2,29 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] players, int m, int k) {
-        List<Queue<Integer>> queues = new ArrayList<>();
+        List<Queue<int[]>> queues = new ArrayList<>();
         queues.add(new LinkedList<>());
         queues.add(new LinkedList<>());
         int answer = 0;
+        int curServerSize = 0;
         for(int i = 0 ; i < players.length ; i++){
             int player = players[i];
-            int requiredServer = player / m;
-            Queue<Integer> curQueue = queues.get(i % 2);
-            Queue<Integer> nextQueue = queues.get((i + 1) % 2);
-            int curServer = curQueue.size();
-            if(requiredServer > curServer){
-                int addServer = requiredServer - curServer;
-                answer += addServer;
+            int requiredServerSize = player / m;
+            Queue<int[]> curQueue = queues.get(i % 2);
+            Queue<int[]> nextQueue = queues.get((i + 1) % 2);
+            if(requiredServerSize > curServerSize){
+                int addServerSize = requiredServerSize - curServerSize;
+                answer += addServerSize;
                 if(k > 1){
-                    for(int j = 0 ; j < addServer ; j++){
-                        nextQueue.add(1);
-                    }
+                    nextQueue.add(new int[]{1, addServerSize});
+                    curServerSize += addServerSize;
                 }
             }
             while(curQueue.size() > 0){
-                int serverUsingTime = curQueue.poll() + 1;
-                if(serverUsingTime < k){
-                    nextQueue.add(serverUsingTime);
+                int[] curServer = curQueue.poll();
+                if(curServer[0] + 1 < k){
+                    nextQueue.add(new int[]{curServer[0] + 1, curServer[1]});
+                }
+                else{
+                    curServerSize -= curServer[1];
                 }
             }
         }
